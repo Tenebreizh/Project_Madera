d8c57160e4a729328f2cbd3ae8e524de96a3deb9
Nathan <nathan.ropars@gmail.com>
2019-09-25 15:05:54 +0200

    router

--- resources/js/router.js
+++ resources/js/router.js
@@ -6,6 +6,7 @@ import ExampleComponent from './components/ExampleComponent.vue'
 import Catalog          from './components/pages/catalog/Index.vue'
 import Customer         from './components/pages/customer/indexCustomer.vue'
 import User             from './components/pages/user/indexUser.vue'
+import Configuration    from './components/pages/configuation/indexConfiguration.vue'
 
 const routes = [
     { name: "home", path: '/', component: ExampleComponent },
@@ -15,6 +16,8 @@ const routes = [
     { name: "customer", path: '/customer', component: Customer},
 
     { name: "user", path: '/user', component: User},
+
+    { name: "configuration", path: '/config', component: Configuration},
 ]
 
 export default new Router({
