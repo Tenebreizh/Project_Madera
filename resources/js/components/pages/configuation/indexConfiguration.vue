994f3e5ab31eeb44fdba90f960d3e7ec01145b3d
Nathan <nathan.ropars@gmail.com>
2019-09-27 20:55:20 +0200

    add modal config module page

--- resources/js/components/pages/configuation/indexConfiguration.vue
+++ resources/js/components/pages/configuation/indexConfiguration.vue
@@ -168,8 +168,8 @@
 
 
         <!-- Modal huisserie -->
-        <div class="modal fade" id="AddWindowFrame" tabindex="-1" role="dialog" aria-labelledby="AddWindowFrameLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
+        <div class="modal fade  bd-example-modal-lg" id="AddWindowFrame" tabindex="-1" role="dialog" aria-labelledby="AddWindowFrameLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="AddWindowFrameLabel">Ajout d'une huisserie</h5>
