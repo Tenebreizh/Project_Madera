566232a3ee6cf2e7e243c9141e01cb1ec1d7b950
MarcDev <marcrousseau77@yahoo.fr>
2019-10-01 11:01:23 +0200

    Modif. page IndexConfiguration
    Création page detailConfiguration.vue

---
+++ resources/js/components/pages/configuation/detailConfiguration.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <form submit.prevent>
+                <div class="form-group">
+                    <label>ID de la configuration</label>
+                    <input type="number" class="form-control" v-model="id" disabled />
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ConfigurationDetail',
+    props: {
+        id: String
+    }
+}
+</script>
