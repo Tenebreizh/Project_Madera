7aaf7c1eaa6c02d89f9d17f567652c00a91de6f5
Unknown <nathan.ropars@gmail.com>
2019-11-18 10:21:47 +0100

    visualisation client + config

--- resources/js/components/pages/configuation/WindowFrameShow.vue
+++ resources/js/components/pages/configuation/WindowFrameShow.vue
@@ -7,11 +7,11 @@
                     <div class="card-body">
                         <div class="col form-group">
                             <label for="reference">Référence:</label>
-                            <input type="text" class="form-control" id="reference" name="reference">
+                            <input type="text" class="form-control" id="reference" name="reference" v-model="windowFrame.reference">
                         </div>
                         <div class="col form-group">
                             <label for="description">Description:</label>
-                            <textarea name="description" class="form-control" id="description"></textarea>
+                            <textarea name="description" class="form-control" id="description" v-model="windowFrame.reference"></textarea>
                         </div>
                         <div class="col-sm-12 text-right">
                             <button type="button" class="btn btn-primary btn-lg">Valider</button>
@@ -25,7 +25,32 @@
 
 <script>
 export default {
+    data(){
+        return{
+            windowFrame:{
+                label:'',
+                description:'',
+                reference:''
+            },
+        }
+    },
 
+    methods:{
+        getwindowFrame(){
+            let id = this.$route.params.id
+            this.loadingData = true
+            axios.get("/api/windowFrame/"+id)
+            .then(response => {
+                this.windowFrame = response.data
+                this.loadingData = false
+            })
+        },
+       
+    },
+
+    mounted() {
+        this.getwindowFrame()
+    },
 }
 </script>
 
