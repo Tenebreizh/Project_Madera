63bfaf2a7d118230058cb3ba3af9c39eab0203ec
Thibaud <thibaud29@protonmail.com>
2020-06-01 16:54:49 +0200

    add specific role to user_types

--- database/seeds/UserTypeSeeder.php
+++ database/seeds/UserTypeSeeder.php
@@ -11,6 +11,32 @@ class UserTypeSeeder extends Seeder
      */
     public function run()
     {
+        
+        factory(App\Models\UserType::class)->create([
+            'name' => 'Commercial',
+            'description' => ''
+        ]);
+
+        factory(App\Models\UserType::class)->create([
+            'name' => "Bureau d'étude",
+            'description' => ''
+        ]);
+            
+        factory(App\Models\UserType::class)->create([
+            'name' => 'Client',
+            'description' => ''
+        ]);
+            
+        factory(App\Models\UserType::class)->create([
+            'name' => 'Admin',
+            'description' => ''
+        ]);
+                
+        factory(App\Models\UserType::class)->create([
+            'name' => 'Comptabilité',
+            'description' => ''
+        ]);
+                    
         factory(App\Models\UserType::class, 10)->create();
     }
 }
