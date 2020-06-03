d8c2eaa25b99ea455de5fce5c32a785b4f70ad6e
Thibaud <thibaud29@protonmail.com>
2020-06-03 11:12:45 +0200

    edit actionSeed & logFactory

--- database/factories/LogFactory.php
+++ database/factories/LogFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Log::class, function (Faker $faker) {
     return [
         'user_id' => factory(App\User::class)->create(),
-        'action_id' => factory(App\Models\Action::class)->create(),
+        'action_id' => mt_rand(1, 4),
         'name' => $faker->text(10),
         'description' => $faker->text(30),
         'table' => $faker->text(15)
