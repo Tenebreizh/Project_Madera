06243c4e030af0a19f664d6cd25f8257f847fe85
Thibaud <thibaud29@protonmail.com>
2020-06-03 16:36:10 +0200

    Log for ExternalFinitionController

--- app/Http/Controllers/ExternalFinitionController.php
+++ app/Http/Controllers/ExternalFinitionController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ExternalFinition;
+use App\Models\Action;
 use Illuminate\Http\Request;
+use App\Models\ExternalFinition;
+use App\Http\Controllers\LogController;
 
 class ExternalFinitionController extends Controller
 {
+    private $table = "external_finitions";
+    private $log;
+
+    public function __construct(LogController $log)
+    {
+        $this->log = $log;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -31,6 +41,14 @@ class ExternalFinitionController extends Controller
             'label' => $request->label,
             'description' => $request->description,
             'reference' => $request->reference
+        ]);
+
+        $this->log->store([
+            "user_id" => $request->user_id,
+            "action_id" => Action::where(['label' => 'Create'])->first()->id,
+            "name" => "Creation",
+            "description" => "Creation d'une finition externe",
+            "table" => $this->table,
         ]);
             
         return $externalFinition;
@@ -59,8 +77,15 @@ class ExternalFinitionController extends Controller
         $externalFinition->label = $request->label;
         $externalFinition->description = $request->description;
         $externalFinition->reference = $request->reference;
-
         $externalFinition->save();
+
+        $this->log->store([
+            "user_id" => $request->user_id,
+            "action_id" => Action::where(['label' => 'Update'])->first()->id,
+            "name" => "Mise à jour",
+            "description" => "Mise à jour d'une finition externe",
+            "table" => $this->table,
+        ]);
 
         return $externalFinition;
     }
@@ -71,9 +96,17 @@ class ExternalFinitionController extends Controller
      * @param  \App\Models\ExternalFinition  $externalFinition
      * @return \Illuminate\Http\Response
      */
-    public function destroy(ExternalFinition $externalFinition)
+    public function destroy(Request $request, ExternalFinition $externalFinition)
     {
         $externalFinition->delete();
+
+        $this->log->store([
+            "user_id" => $request->user_id,
+            "action_id" => Action::where(['label' => 'Delete'])->first()->id,
+            "name" => "Suppression",
+            "description" => "Suppression d'une finition externe",
+            "table" => $this->table,
+        ]);
 
         return response()->json([
                 "message" => "External finition successfully deleted"
